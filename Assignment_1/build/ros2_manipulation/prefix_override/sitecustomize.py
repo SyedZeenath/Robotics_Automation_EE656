802eb72bbada664918179e88f06e8bc562929732
SyedZeenath <s.zeenath.ara@gmail.com>
2025-10-20 21:23:29 +0100

    initial folder structure for ros2_manipulation.

---
+++ Assignment_1/build/ros2_manipulation/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/master26/Robotics_Automation_EE656/Assignment_1/install/ros2_manipulation'
