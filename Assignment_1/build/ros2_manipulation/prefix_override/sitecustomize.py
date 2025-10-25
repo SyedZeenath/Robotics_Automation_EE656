1f8e680fdc353911ab19a5eb24be0719a569dc82
Syed Zeenath <37401872+SyedZeenath@users.noreply.github.com>
2025-10-25 21:05:02 +0100

    Delete Assignment_1/build directory

--- Assignment_1/build/ros2_manipulation/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/master26/Robotics_Automation_EE656/Assignment_1/install/ros2_manipulation'
