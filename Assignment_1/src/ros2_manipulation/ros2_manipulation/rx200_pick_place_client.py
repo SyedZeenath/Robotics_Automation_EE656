e188e9c562b0c0374bf0d5f380ee38c245e8fd04
SyedZeenath <s.zeenath.ara@gmail.com>
2025-11-03 03:51:51 +0000

    Code cleanup

--- Assignment_1/src/ros2_manipulation/ros2_manipulation/rx200_pick_place_client.py
+++ Assignment_1/src/ros2_manipulation/ros2_manipulation/rx200_pick_place_client.py
@@ -34,13 +34,10 @@ class MoveItEEClient(Node):
         self._pick_point = tuple(self.get_parameter('pick_point').value)
         self._place_point = tuple(self.get_parameter('place_point').value)
         self._timeout_sec = self.get_parameter('timeout_sec').value
-
-        self.marker_pub = self.create_publisher(Marker, '/visualization_marker', 10)
-        self.path_points = []
         
         self.get_logger().info('Node initialized successfully!')
     
-    def send_pose(self, x, y, z, w = 0.0):
+    def send_pose(self, x, y, z):
         pose = PoseStamped()
         pose.header.frame_id = self.base_link
         pose.pose.position.x = x
@@ -49,7 +46,7 @@ class MoveItEEClient(Node):
         
         distance = math.sqrt(x**2 + y**2)  # Horizontal distance from base
         # pitch is descided based on how close we are to the object and how low it is
-        if distance < 0.3:
+        if distance <= 0.2:
             pitch = math.pi / 2  # Pointing downwards
         else:
             pitch = 0.0  # Level orientation
@@ -99,7 +96,6 @@ class MoveItEEClient(Node):
         send_future = self._client.send_goal_async(goal, feedback_callback=self._feedback_cb)
         send_future.add_done_callback(self._goal_response_cb)
 
-        return send_future
 
     def send_gr_pose(self, open=True):
         req = MotionPlanRequest()
@@ -129,25 +125,6 @@ class MoveItEEClient(Node):
         # Return the send future to allow optional synchronous waiting by the caller
         return send_future
 
-    def send_pose_and_wait(self, x, y, z, w=0.0, timeout_sec: float = None):
-        """Send a pose goal and block until the goal result is available (or timeout).
-        """
-        send_future = self.send_pose(x, y, z, w)
-        if send_future is None:
-            self.get_logger().error(f"Failed to send pose to ({x}, {y}, {z}) due to unreachable target")
-            return None
-        # Wait until send_future completes (gives a GoalHandle)
-        rclpy.spin_until_future_complete(self, send_future, timeout_sec=timeout_sec)
-        goal_handle = send_future.result()
-        if not goal_handle or not goal_handle.accepted:
-            self.get_logger().error('Failed to get goal handle (send_future returned None)')
-            return None
-
-        # Wait for the result future to complete
-        result_future = goal_handle.get_result_async()
-        rclpy.spin_until_future_complete(self, result_future, timeout_sec=timeout_sec)
-        return result_future.result().result if result_future.done() else None  
-    
     # Callbacks 
     def _goal_response_cb(self, future):
         goal_handle = future.result()
@@ -181,15 +158,13 @@ class MoveItEEClient(Node):
         # 1. Before Pick: A step before the pick point 
         before_pick = Point(x=round(self._pick_point[0] - 0.1, 2), y=self._pick_point[1], z=z)
         self.get_logger().info(f"Moving before PICK point: {before_pick}")
-        if not self.send_pose_and_wait(before_pick.x, before_pick.y, before_pick.z):
-            return
+        self.send_pose(before_pick.x, before_pick.y, before_pick.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=1.0))
         
         # 2. Move to Pick point
         pick_point = Point(x=self._pick_point[0], y=self._pick_point[1], z=z)
         self.get_logger().info(f"MOVING TO PICK point: {pick_point}")
-        if not self.send_pose_and_wait(pick_point.x, pick_point.y, pick_point.z):
-            return
+        self.send_pose(pick_point.x, pick_point.y, pick_point.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))
 
         # 3. Close gripper: hold the object
@@ -200,22 +175,19 @@ class MoveItEEClient(Node):
         # 4. Lift
         above_pick = Point(x=self._pick_point[0], y=round(self._pick_point[1] + 0.15, 2), z=z)
         self.get_logger().info(f"LIFTING: {above_pick}")
-        if not self.send_pose_and_wait(above_pick.x, above_pick.y, above_pick.z):
-            return
+        self.send_pose(above_pick.x, above_pick.y, above_pick.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))
 
         # 5. Above Place point, 0.15 indicate a random lifting height, since z is not used
         above_place = Point(x=self._place_point[0], y=round(self._place_point[1] + 0.15, 2), z=z)
         self.get_logger().info(f"Moving ABOVE PLACE point: {above_place}")
-        if not self.send_pose_and_wait(above_place.x, above_place.y, above_place.z):
-            return
+        self.send_pose(above_place.x, above_place.y, above_place.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))
         
         # 6. Descend to Place
         place_point = Point(x=self._place_point[0], y=self._place_point[1], z=z)
         self.get_logger().info(f"DESCENDING TO PLACE point: {place_point}")
-        if not self.send_pose_and_wait(place_point.x, place_point.y, place_point.z):
-            return
+        self.send_pose(place_point.x, place_point.y, place_point.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))
         
         # 7. Open gripper: release the object
@@ -225,22 +197,16 @@ class MoveItEEClient(Node):
 
         # 8. Lift: Back to above place
         self.get_logger().info(f"LIFTING {above_place}")
-        if not self.send_pose_and_wait(above_place.x, above_place.y, above_place.z):
-            return
+        self.send_pose(above_place.x, above_place.y, above_place.z)
         self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))       
         
-        self.get_logger().info("PICK-AND-PLACE COMPLETE")
-        
-        self.get_logger().info(f"Moving to Home")
-        if not self.send_pose_and_wait(0.0, 0.0, z):
-            return
-        self.get_clock().sleep_for(rclpy.duration.Duration(seconds=2.0))
+        self.get_logger().info("PICK-AND-PLACE COMPLETE")          
+        self.get_clock().sleep_for(rclpy.duration.Duration(seconds=5.0))
 
 def main():
     rclpy.init()
     node = MoveItEEClient()   
     try:
-        # while rclpy.ok():
         node.run()            
     except KeyboardInterrupt:
         node.get_logger().info('Interrupted by user, shutting down')
